c2655e04f41b5b1ef1bfb96ce6e14f8e88a85851
João Marcos Neves da Silva <58483603+joaomarcosns@users.noreply.github.com>
2021-05-18 09:10:29 -0300

    Adicionado conexão com o banco

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<?php
+ include './../app/Libraries/Database.php';
+
+$db = new Database;
+
+$db->query("SELECT * FROM cliente");
+$db->executa();
+
+echo 'total de linhas afetadas: '.$db->totalResultados();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1>teste de banco de dados</h1>
+</body>
+</html>
