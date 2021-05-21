e302623aea81903a94f97567d75dae3b8ee35ce4
João Marcos Neves da Silva <58483603+joaomarcosns@users.noreply.github.com>
2021-05-21 09:40:14 -0300

    Adicionado PagamentoVendaModel

---
+++ app/Models/PagamentoVendaModel.php
@@ -0,0 +1,94 @@
+<?php
+
+class PagamentoVendaModel
+{
+    private VendaModel $venda;
+    private FormaPagamentoModel $formaPagamento;
+    private $numeroDeParcelas;
+    private $valorAPagar;
+    private $valorPago;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+    /**
+     * @return mixed
+     */
+    public function getVenda()
+    {
+        return $this->venda;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFormaPagamento()
+    {
+        return $this->formaPagamento;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNumeroDeParcelas()
+    {
+        return $this->numeroDeParcelas;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValorAPagar()
+    {
+        return $this->valorAPagar;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValorPago()
+    {
+        return $this->valorPago;
+    }
+
+    /**
+     * @param mixed $venda
+     */
+    public function setVenda($venda)
+    {
+        $this->venda = $venda;
+    }
+
+    /**
+     * @param mixed $formaPagamento
+     */
+    public function setFormaPagamento($formaPagamento)
+    {
+        $this->formaPagamento = $formaPagamento;
+    }
+
+    /**
+     * @param mixed $numeroDeParcelas
+     */
+    public function setNumeroDeParcelas($numeroDeParcelas)
+    {
+        $this->numeroDeParcelas = $numeroDeParcelas;
+    }
+
+    /**
+     * @param mixed $valorAPagar
+     */
+    public function setValorAPagar($valorAPagar)
+    {
+        $this->valorAPagar = $valorAPagar;
+    }
+
+    /**
+     * @param mixed $valorPago
+     */
+    public function setValorPago($valorPago)
+    {
+        $this->valorPago = $valorPago;
+    }
+}
