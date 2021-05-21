f2714f68694b556f6861c15438657d397f205d59
João Marcos Neves da Silva <58483603+joaomarcosns@users.noreply.github.com>
2021-05-21 09:32:01 -0300

    Adicionado PagamentoCompraModel

---
+++ app/Models/PagamentoCompraModel.php
@@ -0,0 +1,96 @@
+<?php
+
+class PagamentoCompraModel
+{
+    private CompraModel $compra;
+    private FormaPagamentoModel $formaDePagamento;
+    private $parcelas;
+    private $prazo;
+    private $status;
+
+    public function __construct() 
+    {
+        $this->db = new Database();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCompra()
+    {
+        return $this->compra;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFormaDePagamento()
+    {
+        return $this->formaDePagamento;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getParcelas()
+    {
+        return $this->parcelas;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrazo()
+    {
+        return $this->prazo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $compra
+     */
+    public function setCompra($compra)
+    {
+        $this->compra = $compra;
+    }
+
+    /**
+     * @param mixed $formaDePagamento
+     */
+    public function setFormaDePagamento($formaDePagamento)
+    {
+        $this->formaDePagamento = $formaDePagamento;
+    }
+
+    /**
+     * @param mixed $parcelas
+     */
+    public function setParcelas($parcelas)
+    {
+        $this->parcelas = $parcelas;
+    }
+
+    /**
+     * @param mixed $prazo
+     */
+    public function setPrazo($prazo)
+    {
+        $this->prazo = $prazo;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+}
+
