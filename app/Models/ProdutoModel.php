f0afe2a8e72d1c09be8bdc7c62a327a6967ccffc
João Marcos Neves da Silva <58483603+joaomarcosns@users.noreply.github.com>
2021-05-21 08:33:53 -0300

    Adicionado ProdutoModel

---
+++ app/Models/ProdutoModel.php
@@ -0,0 +1,213 @@
+<?php
+
+class ProdutoModel
+{
+    private CategoriaModel $categoria;
+    private $nome_produto;
+    private $icms;
+    private $ipi;
+    private $frete;
+    private $acrescimo_despesas;
+    private $preco_na_fabrica;
+    private $preco_compra;
+    private $preco_venda;
+    private $lucro;
+    private $desconto;
+    private $quantidade;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+    /**
+     * @return mixed
+     */
+    public function getCategoria()
+    {
+        return $this->categoria;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNome_produto()
+    {
+        return $this->nome_produto;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIcms()
+    {
+        return $this->icms;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIpi()
+    {
+        return $this->ipi;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFrete()
+    {
+        return $this->frete;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAcrescimo_despesas()
+    {
+        return $this->acrescimo_despesas;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPreco_na_fabrica()
+    {
+        return $this->preco_na_fabrica;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPreco_compra()
+    {
+        return $this->preco_compra;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPreco_venda()
+    {
+        return $this->preco_venda;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLucro()
+    {
+        return $this->lucro;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDesconto()
+    {
+        return $this->desconto;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getQuantidade()
+    {
+        return $this->quantidade;
+    }
+
+    /**
+     * @param mixed $categoria
+     */
+    public function setCategoria($categoria)
+    {
+        $this->categoria = $categoria;
+    }
+
+    /**
+     * @param mixed $nome_produto
+     */
+    public function setNome_produto($nome_produto)
+    {
+        $this->nome_produto = $nome_produto;
+    }
+
+    /**
+     * @param mixed $icms
+     */
+    public function setIcms($icms)
+    {
+        $this->icms = $icms;
+    }
+
+    /**
+     * @param mixed $ipi
+     */
+    public function setIpi($ipi)
+    {
+        $this->ipi = $ipi;
+    }
+
+    /**
+     * @param mixed $frete
+     */
+    public function setFrete($frete)
+    {
+        $this->frete = $frete;
+    }
+
+    /**
+     * @param mixed $acrescimo_despesas
+     */
+    public function setAcrescimo_despesas($acrescimo_despesas)
+    {
+        $this->acrescimo_despesas = $acrescimo_despesas;
+    }
+
+    /**
+     * @param mixed $preco_na_fabrica
+     */
+    public function setPreco_na_fabrica($preco_na_fabrica)
+    {
+        $this->preco_na_fabrica = $preco_na_fabrica;
+    }
+
+    /**
+     * @param mixed $preco_compra
+     */
+    public function setPreco_compra($preco_compra)
+    {
+        $this->preco_compra = $preco_compra;
+    }
+
+    /**
+     * @param mixed $preco_venda
+     */
+    public function setPreco_venda($preco_venda)
+    {
+        $this->preco_venda = $preco_venda;
+    }
+
+    /**
+     * @param mixed $lucro
+     */
+    public function setLucro($lucro)
+    {
+        $this->lucro = $lucro;
+    }
+
+    /**
+     * @param mixed $desconto
+     */
+    public function setDesconto($desconto)
+    {
+        $this->desconto = $desconto;
+    }
+
+    /**
+     * @param mixed $quantidade
+     */
+    public function setQuantidade($quantidade)
+    {
+        $this->quantidade = $quantidade;
+    }
+}
