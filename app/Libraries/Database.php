bf8c3d0465e212844ede32714641f288fb7af3bc
João Marcos Neves da Silva <58483603+joaomarcosns@users.noreply.github.com>
2021-05-24 21:59:42 -0300

    Banco

---
+++ app/Libraries/Database.php
@@ -0,0 +1,91 @@
+<?php
+
+include '../config.php';
+
+class Database {
+
+    private $host = HOST;
+    private $usuario = USER;
+    private $senha = PASSWORD;
+    private $banco = BANK;
+    private $porta = PORT;
+    private $dbh;
+    private $stmt;
+
+    public function __construct()
+    {
+        //fonte de dados ou DSN contém as informações necessárias para conectar ao banco de dados.
+        $dsn = 'pgsql:host='.$this->host.';port='.$this->porta.';dbname='.$this->banco;
+        $opcoes = [
+            //armazena em cache a conexão para ser reutilizada, evita a sobrecarga de uma nova conexão, resultando em um aplicativo mais rápido
+            PDO::ATTR_PERSISTENT => true,
+            //lança uma PDOException se ocorrer um erro 
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ];
+        try {
+            //cria a instancia do PDO
+            $this->dbh = new PDO($dsn, $this->usuario, $this->senha, $opcoes);
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+
+    //Prepared Statements com query
+    public function query($sql){
+        //prepara uma consulta sql
+        $this->stmt = $this->dbh->prepare($sql);
+    }
+
+    //vincula um valor a um parâmetro
+    public function bind($parametro, $valor, $tipo = null){
+        if(is_null($tipo)):
+            switch (true):
+                case is_int($valor):
+                    $tipo = PDO::PARAM_INT;
+                break;
+                case is_bool($valor):
+                    $tipo = PDO::PARAM_BOOL;
+                break;
+                case is_null($valor):
+                    $tipo = PDO::PARAM_NULL;
+                break;
+                default:
+                $tipo = PDO::PARAM_STR;
+            endswitch;
+        endif;
+
+        $this->stmt->bindValue($parametro, $valor, $tipo);
+    }
+
+    //executa prepared statement
+    public function executa(){
+        return $this->stmt->execute();
+    }
+
+    //obtem um único registro
+    public function resultado(){
+        $this->executa();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+    //obtem um conjunto de registros
+    public function resultados(){
+        $this->executa();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    //retorna o número de linhas afetadas pela última instrução SQL
+    public function totalResultados(){
+        return $this->stmt->rowCount();
+    }
+
+    //retorna o último ID inserido no banco de dados
+    public function ultimoIdInserido(){
+        return $this->dbh->lastInsertId();
+    }
+
+}
+
+
+
--- app/Libraries/Rota.php
+++ app/Libraries/Rota.php
@@ -1,12 +1,49 @@
 <?php
+/*
+   * Classe Rota
+   * Cria as URL, carrega os controladores, métodos e parametros
+   * FORMATO URL - /controlador/metodo/parametros
+*/
 
 class Rota {
+    //atributos da classe
+    private $controlador = 'Paginas';
+    private $metodo = 'index';
+    private $parametros = [];
 
     public function __construct()
     {
-       $this->url();
+        //se a url existir joga a funcao url na variavel $url
+       $url = $this->url() ? $this->url() : [0];
+        //checa se o controlador existe
+        //ucwords — Converte para maiúsculas o primeiro caractere de cada palavra
+       if(file_exists('../app/Controllers/'.ucwords($url[0]).'.php')):
+            //se existir seta como controlador
+            $this->controlador = ucwords($url[0]);
+            //unset — Destrói a variável especificada
+            unset($url[0]);
+       endif;
 
-       var_dump($this->url());
+       //requere o controlador
+       require_once '../app/Controllers/'.$this->controlador.'.php';
+       //instancia o controlador
+       $this->controlador = new $this->controlador;
+
+       //checa se o método existe, segunda parte da url
+       if(isset($url[1])):
+            //method_exists — Checa se o método da classe existe
+            if(method_exists($this->controlador, $url[1])):
+               $this->metodo = $url[1];
+               unset($url[1]);     
+            endif;
+       endif;
+
+        //Se existir retorna um array com os valores se não retorna um array vazio
+        //array_values — Retorna todos os valores de um array
+       $this->parametros = $url ? array_values($url) : [];
+       //call_user_func_array — Chama uma dada função de usuário com um array de parâmetros
+       call_user_func_array([$this->controlador, $this->metodo], $this->parametros);
+
     }
 
     // retorna a url em um array
