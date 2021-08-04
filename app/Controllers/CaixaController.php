0a7c68c63289626639c46490e0ba6dec156e7799
João Marcos Neves da Silva <58483603+joaomarcosns@users.noreply.github.com>
2021-08-04 08:52:47 -0300

    Cadastarndo caixa

--- app/Controllers/CaixaController.php
+++ app/Controllers/CaixaController.php
@@ -9,11 +9,8 @@ class CaixaController extends Controller
             // URL::redirecionar('FuncionarioController/login');
         endif;
         $this->caixaModel = $this->model('CaixaModel');
+        
 
-    }
-
-    public function index()
-    {
     }
     public function listarCaixa()
     {
@@ -25,26 +22,36 @@ class CaixaController extends Controller
 
     public function cadastrar()
     {
+        $imgSuccess = '<img id="success" src="../public/img/check-icon.svg" alt="Sucesso">';
+        $imgError = '<img id="error" src="../public/img/block-icon.svg" alt="Erro">';
 
         $formulario = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         if (isset($formulario)) :
             $dados = [
                 'valor_em_caixa' => trim($formulario['valor_em_caixa']),
-
+                'id_funcionario' => trim($formulario['id_funcionario']),
                 'valor_em_caixa_erro' => '',
             ];
 
             if (in_array("", $formulario)) :
 
                 if (empty($formulario['valor_em_caixa'])) :
-                    $dados['valor_em_caixa_erro'] = "Preencha o campo";
+                    Sessao::mensagem('caixa', 'Preencha o campo valor em caixa!' . $this->$imgError, 'bg-red');
+                    header("Location:" . URL . DIRECTORY_SEPARATOR . 'CaixaController/listarCaixas');
                 endif;
+
+                if (empty($formulario['id_funcionario'])) :
+                    Sessao::mensagem('caixa', 'Preencha o campo Funcionario!' . $this->$imgError, 'bg-red');
+                    header("Location:" . URL . DIRECTORY_SEPARATOR . 'CaixaController/listarCaixas');
+                endif;
+
             else:
                 if (Validar::validarCampoNumerico($formulario['valor_em_caixa'])) :
-                    $dados['valor_em_caixa_erro'] = "Nome informado é <b>invalido</b>";
+                    Sessao::mensagem('caixa', 'Formato informado Invalido!' . $this->$imgError, 'bg-red');
+                    header("Location:" . URL . DIRECTORY_SEPARATOR . 'CaixaController/listarCaixas');
                 else:
                     if ($this->caixaModel->insert($dados)) :
-                        echo 'Cadastro realizado como sucesso <hr>';
+                        Sessao::mensagem('caixa', 'Caixa Cadastrado com sucesso!' . $this->$imgSuccess, 'bg-green');
 
                     else :
                         die("Erro");
