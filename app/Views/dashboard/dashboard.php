2dae55a621f4529907760ecbe2dc307843dd4c3d
netosep <clementesepulveda27@gmail.com>
2021-08-12 09:37:59 -0300

    Refatorando style

--- app/Views/dashboard/dashboard.php
+++ app/Views/dashboard/dashboard.php
@@ -8,7 +8,16 @@
     <link rel="shortcut icon" href="<?= URL ?>/public/img/favicon.svg" type="image/x-icon">
     <title>SISCONVE - Dashboard</title>
     <!-- estilos -->
-    <?php include("./../app/Include/etc/styles.php"); ?>
+    <link rel="stylesheet" href="<?= URL ?>/public/style/navbar.css">
+    <link rel="stylesheet" href="<?= URL ?>/public/style/main.css">
+    <link rel="stylesheet" href="<?= URL ?>/public/style/sidebar.css">
+    <link rel="stylesheet" href="<?= URL ?>/public/style/box-center.css">
+    <link rel="stylesheet" href="<?= URL ?>/public/style/dashboard.css">
+    <link rel="stylesheet" href="<?= URL ?>/public/style/content-box.css">
+    <link rel="stylesheet" href="<?= URL ?>/public/style/toast.css">
+    <link rel="stylesheet" href="<?= URL ?>/public/style/modal/modal.css">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
 </head>
 
 <body>
